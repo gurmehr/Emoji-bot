8760cd11c912cc16c7bc03d480c8474340b5904a
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-06 16:20:10 +0530

    first

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v)q!ix+itp&7)#bqw&cj(08b19^+^7&p2gxczsqrd5+-g^c8er'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
