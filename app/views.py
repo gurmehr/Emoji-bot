485983c0fca2172ae442755392d0f8370529b6a7
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-07 11:45:55 +0530

    f

--- app/views.py
+++ app/views.py
@@ -38,7 +38,13 @@ def post_facebook_message(fbid, recevied_message):
             joke_text = 'Yo ' + reply_text
     
     else:
-        joke_text = 'Yo'
+        try:
+            user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid
+            user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN}
+            user_details = requests.get(user_details_url, user_details_params).json()
+            joke_text = 'Yo '+user_details['first_name']+'..! ' + reply_text
+        except:
+            joke_text = 'Yo ' + reply_text
     
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
