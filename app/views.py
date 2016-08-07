b4bd4b7c4f04cbd0a3015c6415ddbbec43bf7d15
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-07 13:49:38 +0530

    f

--- app/views.py
+++ app/views.py
@@ -72,10 +72,20 @@ def post_facebook_message(fbid, recevied_message):
             joke_text = 'Yo ' + reply_text
 
     joke_text=quote_search(recevied_message)
-
+    message_object = {
+        "attachment":{
+            "type":"image",
+                "payload":{
+                    #"url":"http://thecatapi.com/api/images/get?format=src&type=png"
+                    "url" : "http://worldversus.com/img/ironman.jpg"
+                    }
+        }
+    }
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
+    response_msg2 = json.dumps({"recipient":{"id":fbid}, "message":message_object})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
     pprint(status.json())
 
 
