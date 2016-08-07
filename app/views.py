6ef75efa611e742b6e6885024adfc5d958292b53
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-07 13:18:20 +0530

    f

--- app/views.py
+++ app/views.py
@@ -25,9 +25,9 @@ quotes_string = '''
 
 
 
-quotes_arr = [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
-              ["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
-              ["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+quotes_arr = ["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse",
+              "Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill",
+              "Strive not to be a success, but rather to be of value.", "Albert Einstein",
               ]
 
 
