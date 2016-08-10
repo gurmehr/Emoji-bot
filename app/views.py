432cccb42d3d73aad12669cf762b9d371cf0109b
Gurmehr Sohi <gurusohi@Gurmehrs-MacBook-Pro.com>
2016-08-10 17:25:07 +0530

    second

--- app/views.py
+++ app/views.py
@@ -85,7 +85,7 @@ def get_emoji(str_var):
     if str_var.lower() in "*,random,anything":
         random.shuffle(emoji_arr)
         return emoji_arr[0][0]
-    arr.append("Here you go - ")
+    arr.append("")
     try0=[['smile','smiling'],['sad','disappointed'],['shocked','flushed'],['potty','poo'],['tatti','poo']]
     k=str_var.split(' ')
     for i in k:
@@ -100,7 +100,7 @@ def get_emoji(str_var):
 
     if flag == 1:
         return " ".join(arr[:10])
-    return "not found"
+    return "Sorry not found!!!"
     
 
 
@@ -124,20 +124,7 @@ def post_facebook_message(fbid, recevied_message):
 
 # joke_text=quote_search(recevied_message)
     joke_text=get_emoji(recevied_message)
-    if(joke_text=="not found"):
-        message_object = {
-            "error":{
-                "message": "No matching emoji found.",
-                "type": "OAuthException",
-                "code": 100,
-                "fbtrace_id": "BLBz/WZt8dN"
-                    }
-
-                }
-        response_msg2 = json.dumps({"recipient":{"id":fbid}, "message":message_object})
-        status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg2)
-        pprint(status.json())
-        return
+    
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
